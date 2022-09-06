fc86631cc47f5e85e2dcd89f689cabc6a4e3600a
NamrataMishra2004 <112928689+NamrataMishra2004@users.noreply.github.com>
2022-09-06 12:18:34 +0530

    Add files via upload

---
+++ SampleForBitrise/SampleForBitrise/SampleForBitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleForBitriseApp.swift
+//  SampleForBitrise
+//
+//  Created by Gamestester8 on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleForBitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
